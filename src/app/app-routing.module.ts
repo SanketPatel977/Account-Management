5c1b2842298fe40265850a51f82f9ac8c2c4270c
SanketPatel977 <125577011+SanketPatel977@users.noreply.github.com>
2023-04-10 22:22:58 +0530

    final project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { InsertComponent } from './insert/insert.component';
 import { UpdateComponent } from './update/update.component';
 import { ViewComponent } from './view/view.component';
 import { DeleteComponent } from './delete/delete.component';
-
+import { BaseComponent } from './base/base.component';
 const routes: Routes = [  
   {path: 'insert' ,component:InsertComponent},
+  {path: 'base' ,component:BaseComponent},
   {path: 'view' ,component:ViewComponent},
   {path: 'delete' ,component:DeleteComponent},
   {path: 'update' ,component:UpdateComponent},
